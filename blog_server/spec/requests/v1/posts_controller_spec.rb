e77e7a955eb9d4406d984bc00a8d0f4f00abe0a9
sudhansh2000 <sudhanshjawale@gmail.com>
2025-05-02 13:38:28 +0530

    added create post test case

--- blog_server/spec/requests/v1/posts_controller_spec.rb
+++ blog_server/spec/requests/v1/posts_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "V1::PostsController", type: :request do
   let!(:category) { create(:category) }
 let!(:user2) { create(:user) }
-let!(:post) { create(:post, user: user2, category: category) }
+let!(:post1) { create(:post, user: user2, category: category) }
 
   describe "create" do
     it "show all post" do
@@ -11,7 +11,7 @@ let!(:post) { create(:post, user: user2, category: category) }
         expect(response).to have_http_status(:ok)
         json = JSON.parse(response.body)
         expect(json.length).to be >= 1
-        expect(json[0]["id"]).to eq(post.id)
+        expect(json[0]["id"]).to eq(post1.id)
     end
 
     it "show all the posts of category" do
@@ -19,7 +19,7 @@ let!(:post) { create(:post, user: user2, category: category) }
         expect(response).to have_http_status(:ok)
         json = JSON.parse(response.body)
         expect(json.length).to be >= 1
-        expect(json[0]["title"]).to eq(post.title)
+        expect(json[0]["title"]).to eq(post1.title)
     end
 
     it "show all the posts of perticular user" do
@@ -28,49 +28,49 @@ let!(:post) { create(:post, user: user2, category: category) }
         json = JSON.parse(response.body)
         expect(json.length).to be >= 1
         # puts json
-        expect(json[0]["title"]).to eq(post.title)
+        expect(json[0]["title"]).to eq(post1.title)
     end
   end
 
   describe "show" do
     it "show post method" do
-        get "/v1/posts/#{post.id}"
+        get "/v1/posts/#{post1.id}"
         expect(response).to have_http_status(:ok)
         json = JSON.parse(response.body)
         expect(json).to be_a(Hash)
-        expect(json["id"]).to eq(post.id)
-      expect(json["title"]).to eq(post.title)
+        expect(json["id"]).to eq(post1.id)
+      expect(json["title"]).to eq(post1.title)
     end
   end
 
-  # describe "create" do
-  #   it "create post" do
-  #     params = {
-  #       post: {
-  #         # user_id: 1,
-  #         category_id: 1,
-  #         title: "new post",
-  #         content: "new post content",
-  #         is_private: false,
-  #         tags: [ "tag1", "tag2" ]
-  #       }
-  #     }
-  #     post "/v1/users/1/posts", params: params
-  #     # expect(response).to have_http_status(:created)
-  #     # json = JSON.parse(response.body)
-  #     # expect(json["post"]["title"]).to eq("new post")
-  #   end
-  # end
+  describe "create" do
+    it "create post" do
+      params = {
+        post: {
+          user_id: user2.id,
+          category_id: category.id,
+          title: "new post",
+          content: "new post content",
+          is_private: false,
+          tags: [ "tag1", "tag2" ]
+        }
+      }
+      post "/v1/users/#{user2.id}/posts", params: params
+      expect(response).to have_http_status(:created)
+      json = JSON.parse(response.body)
+      expect(json["post"]["title"]).to eq("new post")
+    end
+  end
 
   describe "update" do
     it "update post" do
       params = {
         post: {
-          post_id: post.id,
+          post_id: post1.id,
           title: "updated post"
         }
       }
-      patch "/v1/posts/#{post.id}", params: params
+      patch "/v1/posts/#{post1.id}", params: params
       expect(response).to have_http_status(:ok)
       json = JSON.parse(response.body)
       expect(json["message"]).to eq("Post updated successfully")
@@ -79,7 +79,7 @@ let!(:post) { create(:post, user: user2, category: category) }
 
   describe "delete" do
     it "delete post" do
-      delete "/v1/posts/#{post.id}"
+      delete "/v1/posts/#{post1.id}"
       expect(response).to have_http_status(:ok)
       json = JSON.parse(response.body)
       expect(json["message"]).to eq("Post deleted successfully")
